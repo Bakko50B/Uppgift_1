c638aaa56f1494b84386d6b84f9586c2dfffadb3
Torbjörn Lundberg <torbjorn@basstrombone.info>
2025-01-22 21:05:04 +0100

    Datum i js filen. Styling i mobilstorlek

--- src/js/parcel.js
+++ src/js/parcel.js
@@ -1,3 +1,12 @@
 "use strict";
 
-console.log("Hej från Parcel");
+const MyDateEl = document.getElementById("date");
+const today = new Date();
+const date = String(today.getDate()).padStart(2, '0');
+const month = String(today.getMonth() + 1).padStart(2, '0'); // Månaderna är 0-indexerade, så vi lägger till 1
+const year = today.getFullYear();
+
+const fullDate = `${year}-${month}-${date}`;
+
+
+MyDateEl.innerHTML = "Dagens datum: " + fullDate;
